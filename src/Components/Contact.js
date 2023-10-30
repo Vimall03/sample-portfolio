012f99a46cd824d967135a28431f2931860ed729
Vimal M <109758195+Vimall03@users.noreply.github.com>
2023-10-30 23:24:06 +0530

    upload

---
+++ src/Components/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations.
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="mailto:youremail@example.com">youremail@example.com</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/vimall03/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/vimall03" target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
